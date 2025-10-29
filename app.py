64900de448c495aefe56d5c9f6bda553b62b969f
Ethan Karasha <ethankarasha@wsu.edu>
2025-10-29 12:46:19 -0700

    linting

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "<h1>￿ Hello from Flask CI/CD Demo v1!</h1>" \
-    "<p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
+        "<p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
 
 
 if __name__ == "__main__":
