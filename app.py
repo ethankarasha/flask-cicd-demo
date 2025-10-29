6147f7b9ac6fef62fd0f10e07f864bfd90864136
Ethan Karasha <ethankarasha@wsu.edu>
2025-10-29 12:42:09 -0700

    I hate linting

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>￿ Hello from Flask CI/CD Demo v1!</h1><p>Automatic Build → Test → Deploy via " \
-    "GitHub Actions.</p>"
+    return "<h1>￿ Hello from Flask CI/CD Demo v1!</h1>" \
+    "<p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
 
 
 if __name__ == "__main__":
